cc3739691023f82c86590e9479067460da8d248f
Joe Shepard <shepard.joseph.m@gmail.com>
2022-08-26 10:45:34 -0700

    brought in the dependancies that will be needed

---
+++ web-app.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.ennsemble import RandomForestClassifier
+from PIL import Image
+import streamlit as st
